568f46ec263817025614041a0ef01113ee33dd67
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2023-05-24 16:32:17 +0700

    Improve Python code

--- python/main.py
+++ python/main.py
@@ -4,15 +4,9 @@ import sys
 async def perform_task():
     await asyncio.sleep(10)
 
-
-async def main(num_tasks):
-    tasks = []
-
-    for task_id in range(num_tasks):
-        task = asyncio.create_task(perform_task())
-        tasks.append(task)
-
-    await asyncio.gather(*tasks)
+async def main(num_tasks: int):
+    gen = (asyncio.create_task(perform_task()) for i in range(num_tasks))
+    await asyncio.gather(*gen)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
